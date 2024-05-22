827bdebe3c9607736dda8e937cefc9e6c4b83af6
Felipe Melo <felipe.ird@gmail.com>
2024-05-22 17:31:11 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value; 
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do numero: ${de}`);
+    alert(`Até o numero: ${ate}`);
+}
