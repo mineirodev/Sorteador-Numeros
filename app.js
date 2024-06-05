cf5564e57bc98294c075cbe8378b2d5dda771e45
Felipe Melo <felipe.ird@gmail.com>
2024-06-05 17:36:08 -0400

    Fix: Adiciona comentarios no codigo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function sortear() {
+function sortear() { //Função para funcionamento do botão sortear
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value); 
@@ -6,30 +6,31 @@ function sortear() {
     let resultado = [];
     let range = ate-de;
 
+    //Condição para criação de um array sequencial do tamanho do range escolhido
     for (let i = 0; i < range+1; i++){
         numeroSorteado[i] = de++;
     }
-    console.log(numeroSorteado);
-
-    let arrayEmbaralhado = embaralhaArray(numeroSorteado);
-    console.log(arrayEmbaralhado);
-
+  
+    let arrayEmbaralhado = embaralhaArray(numeroSorteado); // Embaralha aleatoriamente o array sequencial criado
+    
+    //atribui o array embaralhado a um array de resultado baseado no quantidada de numeros sorteados escolhidos
     for (i = 0; i < quantidade; i++) {
         resultado[i] = arrayEmbaralhado[i];
     }
-    console.log(resultado);
-    exibirNaTela(resultado);
 
+    exibirNaTela(resultado); //Exibe na ela o resultadp
+    
+    //Ativa o botão reiniciar
     document.getElementById("btn-reiniciar").classList.remove("container__botao-desabilitado");
     document.getElementById("btn-reiniciar").classList.add("container__botao");  
 }
 
-function exibirNaTela(campo) {
+function exibirNaTela(campo) { // Funçao que exibe na tela o resultados
     texto = document.getElementById("result");
     texto.innerHTML =  `Números sorteados: ${campo}`;
 }
 
-function embaralhaArray (array){
+function embaralhaArray (array){ //Função para embaralhar um array
     for (let i = array.length - 1; i >= 0; i--) {
         const j = Math.floor(Math.random() * (i)+1);
         [array[i], array[j]] = [array[j], array[i]];
@@ -37,7 +38,7 @@ function embaralhaArray (array){
     return array;
 }
 
-function limpaCampo() {
+function limpaCampo() { // Funçao que limpa as entradas dos inputs
     quantidade = document.getElementById('quantidade');
     quantidade.value = '';
     de = document.getElementById('de');
@@ -46,10 +47,11 @@ function limpaCampo() {
     ate.value = ''; 
 }
 
-function reiniciar() {
+function reiniciar() { //Função para funcionamento do botão reiniciar
     
     limpaCampo();
-    exibirNaTela("nenhum até agora");
+    exibirNaTela("nenhum até agora"); //Reinicia o resultado mostrado na tela
+    //Desativa o botão reiniciar
     document.getElementById("btn-reiniciar").classList.remove("container__botao");
     document.getElementById("btn-reiniciar").classList.add("container__botao-desabilitado");  
 }
